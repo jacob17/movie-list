5a2a0b33ccdb27d6c98f9f7cfa549ca154f48dbb
jacob17 <jakob17chen@gmail.com>
2022-06-03 20:27:50 +0800

    Project Start

---
+++ favorite.js
@@ -0,0 +1,134 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+const movies = JSON.parse(localStorage.getItem('favoriteMovies'))
+
+const dataPanel = document.querySelector('#data-panel')
+
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input') //新增這裡
+
+const MOVIES_PER_PAGE = 12
+
+const paginator = document.querySelector('#paginator')
+
+renderMovieList(getMoviesByPage(1))
+renderPaginator(movies.length)
+
+// 監聽 data panel
+dataPanel.addEventListener('click', (event) => {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(Number(event.target.dataset.id))
+    //修改以下
+  } else if (event.target.matches('.btn-remove-favorite')) {
+    removeFromFavorite(Number(event.target.dataset.id))
+  }
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  //取消預設事件
+  event.preventDefault()
+  //取得搜尋關鍵字
+  const keyword = searchInput.value.trim().toLowerCase()
+  //儲存符合篩選條件的項目
+  let filteredMovies = []
+  //條件篩選
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+  if (filteredMovies.length === 0) {
+    return alert(`您輸入的關鍵字：${keyword} 沒有符合條件的電影`)
+  }
+  //重新輸出至畫面
+  renderMovieList(filteredMovies)
+  //錯誤處理：輸入無效字串
+  if (!keyword.length) {
+    return alert('請輸入有效字串！')
+  }
+})
+
+paginator.addEventListener('click', function onPaginatorClicked(event) {
+  //如果被點擊的不是 a 標籤，結束
+  if (event.target.tagName !== 'A') return
+
+  //透過 dataset 取得被點擊的頁數
+  const page = Number(event.target.dataset.page)
+  //更新畫面
+  renderMovieList(getMoviesByPage(page))
+})
+
+function renderMovieList(data) {
+  let rawHTML = ''
+  data.forEach((item) => {
+    // title, image, id 隨著每個 item 改變
+    rawHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${POSTER_URL + item.image
+      }" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-danger btn-remove-favorite" data-id="${item.id}">X</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+  })
+  dataPanel.innerHTML = rawHTML
+}
+
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image
+      }" alt="movie-poster" class="img-fluid">`
+  })
+}
+
+function removeFromFavorite(id) {
+  console.log(movies)
+  if (!movies || !movies.length) return
+
+  //透過 id 找到要刪除電影的 index
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+  if (movieIndex === -1) return
+
+  //刪除該筆電影
+  movies.splice(movieIndex, 1)
+
+  //存回 local storage
+  localStorage.setItem('favoriteMovies', JSON.stringify(movies))
+
+  //更新頁面
+  renderMovieList(movies)
+}
+
+function getMoviesByPage(page) {
+  //計算起始 index 
+  const startIndex = (page - 1) * MOVIES_PER_PAGE
+  //回傳切割後的新陣列
+  return movies.slice(startIndex, startIndex + MOVIES_PER_PAGE)
+}
+
+function renderPaginator(amount) {
+  //計算總頁數
+  const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
+  //製作 template 
+  let rawHTML = ''
+
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
+  }
+  //放回 HTML
+  paginator.innerHTML = rawHTML
+}
